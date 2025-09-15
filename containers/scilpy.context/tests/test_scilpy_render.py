c47eb34a0a480d197dbc124638bb1273bbb4f454
arnaudbore <arnaud.bore@gmail.com>
2025-09-15 09:25:52 -0400

    fix env var + fix tests

--- containers/scilpy.context/tests/test_scilpy_render.py
+++ containers/scilpy.context/tests/test_scilpy_render.py
@@ -5,7 +5,7 @@ from tempfile import TemporaryDirectory
 def test_scilpy_gradient_render(script_runner):
     with TemporaryDirectory() as test_dir:
         res1 = script_runner.run(
-            "scil_generate_gradient_sampling.py",
+            "scil_generate_gradient_sampling",
             "10", "10", "10",
             "sampling",
             "--bvals", "1000", "2000", "3000",
@@ -16,7 +16,7 @@ def test_scilpy_gradient_render(script_runner):
         assert res1.success, res1.stderr
 
         res2 = script_runner.run(
-            "scil_visualize_gradients.py",
+            "scil_visualize_gradients",
             "--in_gradient_scheme",
             "sampling.bval", "sampling.bvec",
             "--out_basename", "sampling_snap",
